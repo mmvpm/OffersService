fef92323f8cdd33c27960961ceef9abfbb33f8fd
mmvpm <stroganov.n.36@gmail.com>
2024-04-19 23:04:53 +0300

    Add offers.source column (#25)

--- common/src/main/scala/com/github/mmvpm/model/Offer.scala
+++ common/src/main/scala/com/github/mmvpm/model/Offer.scala
@@ -2,4 +2,10 @@ package com.github.mmvpm.model
 
 import com.github.mmvpm.model.OfferStatus.OfferStatus
 
-case class Offer(id: OfferID, userId: UserID, description: OfferDescription, status: OfferStatus)
+case class Offer(
+    id: OfferID,
+    userId: UserID,
+    description: OfferDescription,
+    status: OfferStatus,
+    source: Option[String]
+)
---
+++ service/src/main/resources/db.migration/V2__add_offers_source.sql
@@ -0,0 +1,1 @@
+alter table offers add column source text default null;
